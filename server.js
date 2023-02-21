070306b5a84c34208b1c4f72ad761b0f4e412f3d
264064 <bobby.aquino@telusinternational.com>
2023-02-21 14:55:18 +0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const http = require("http")
+const app = express()
+const server = http.createServer(app)
+const io = require("socket.io")(server, {
+    cors: {
+        origin: ["http://localhost:3007", "http://localhost:3000"],
+        methods: ["GET", "POST"]
+    }
+})
+
+
+io.on("connection", (socket) => {
+
+
+    socket.on("test", function (msg) {
+        io.emit("testServer", msg)
+    })
+
+
+
+
+
+
+})
+
+server.listen(5000, () => console.log("server is running on port 5000"))
+
