df020d9e52333b3ad7bef5db8c8aec7abb0f785b
264064 <bobby.aquino@telusinternational.com>
2023-02-21 15:17:28 +0800

    test http

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const http = require("http")
+const http = require("https")
 const app = express()
 const server = http.createServer(app)
 const io = require("socket.io")(server, {
