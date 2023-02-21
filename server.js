97512332ad3bad35693d2765de6871ccc5200782
264064 <bobby.aquino@telusinternational.com>
2023-02-21 21:51:40 +0800

    tessst

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const http = require("http")
 const server = http.createServer(app)
-const { Server } = require("socket.io")
+const Server = require("socket.io")
 
 const io = new Server(server)
 
