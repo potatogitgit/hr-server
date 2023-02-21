e86b92cdf4c2668164902230ae2c40bb6bbab14e
264064 <bobby.aquino@telusinternational.com>
2023-02-21 21:59:09 +0800

    tesssst

--- server.js
+++ server.js
@@ -2,26 +2,25 @@ const express = require("express")
 const app = express()
 const http = require("http")
 const server = http.createServer(app)
-const Server = require("socket.io")
 
-const io = new Server(server)
-
-// const io = require("socket.io")(server, {
-//     cors: {
-//         origin: '*',
-//         methods: ['GET', 'POST'],
-//         header: 'Access-Control-Allow-Origin'
-//     }
-// })
+const io = require("socket.io")(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+        header: 'Access-Control-Allow-Origin'
+    }
+})
 
 
 io.on("connection", (socket) => {
+
 
     socket.on("test", function (msg) {
 
         console.log(msg.name)
         io.emit("testServer", msg)
     })
+
 
 })
 
