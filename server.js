8c6d6efa791fc7dd04a6638bc50f4c8af0defb4c
264064 <bobby.aquino@telusinternational.com>
2023-02-22 17:06:01 +0800

    add site

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ io.on("connection", (socket) => {
 
         console.log(msg)
 
-        io.emit("fromserver", msg)
+        io.emit("fromserver", { msg })
         //socket.emit("fromserver", msg)
 
     })
