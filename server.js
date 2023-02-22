82adf581cc4c15b4c9b9229f717db855e7e494d8
264064 <bobby.aquino@telusinternational.com>
2023-02-22 09:43:57 +0800

    delete vercel

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express")
-const app = express()
 const http = require("http")
+const app = express()
 const server = http.createServer(app)
+
 
 const io = require("socket.io")(server, {
     cors: {
-        origin: '*',
+        origin: ['http://localhost:3000', 'http://localhost:3000'],
         methods: ['GET', 'POST'],
         header: 'Access-Control-Allow-Origin'
     }
@@ -14,19 +15,19 @@ const io = require("socket.io")(server, {
 
 io.on("connection", (socket) => {
 
-
     socket.on("test", function (msg) {
 
-        console.log(msg.name)
+        console.log("Test: " + msg)
+
         io.emit("testServer", msg)
     })
-
 
 })
 
 app.get("/", (req, res) => {
-    res.send('This is working')
+    res.send('This is totally working')
 })
+
 
 server.listen(5000, () => console.log("server is running on port 5000"))
 
